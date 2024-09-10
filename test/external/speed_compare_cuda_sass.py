e1aa5e675b9ec72d1446edcc4c2507faa47ee03e
juliangrohmann <juliangrohmann@protonmail.com>
2024-09-10 05:01:37 +0400

    compare speed

--- test/external/speed_compare_cuda_sass.py
+++ test/external/speed_compare_cuda_sass.py
@@ -1,130 +1,51 @@
-import json, tempfile, hashlib, subprocess, traceback
-from collections import defaultdict
-from io import BytesIO
-from copy import deepcopy
-from pathlib import Path
-import numpy as np
+import itertools
 from tinygrad import Device
-from tinygrad.device import Buffer
 from tinygrad.engine.realize import CompiledRunner
-from tinygrad.helpers import getenv, to_function_name, colored
-from tinygrad.dtype import dtypes
+from tinygrad.helpers import getenv, colorize_float
 from extra.optimization.helpers import load_worlds, ast_str_to_lin
 from tinygrad.engine.search import bufs_from_lin
-from tinygrad.runtime.support.compiler_cuda import CUDACompiler, SASSCompiler
-from tinygrad.runtime.support.elf import elf_loader, make_elf
-from tinygrad.engine.graph import graph_uops
-import tinygrad.runtime.autogen.libc as libc
-from tinygrad.renderer.sass import SASSRenderer
-from CuAsm.CuAsmLogger import CuAsmLogger
-from CuAsm import CuAsmParser, CubinFile
-
-def info(bufs_cuda, bufs_sass):
-  ret = []
-  for cuda, sass in zip(bufs_cuda, bufs_sass):
-    b_info = {}
-    cuda_vals, sass_vals = [np.frombuffer(buf.as_buffer(), dtype=np.dtype(buf.dtype.fmt)) for buf in [cuda, sass]]
-    mask = ~(np.isnan(cuda_vals) & np.isnan(sass_vals))
-    b_info["cuda"], b_info["sass"] = cuda_vals.tolist(), sass_vals.tolist()
-    try:
-      b_info["sum_diff"] = np.abs(np.sum(cuda_vals[mask] - sass_vals[mask])).item()
-      b_info["max_diff"] = np.max(np.abs(cuda_vals[mask] - sass_vals[mask])).item()
-    except Exception as e:
-      pass
-    ret.append(b_info)
-  return ret
-
-def allclose(bufs_a, bufs_b):
-  for a, b in zip(bufs_a, bufs_b):
-    cuda_vals, sass_vals = [np.frombuffer(buf.as_buffer(), dtype=np.dtype(buf.dtype.fmt)) for buf in [a, b]]
-    mask = ~(np.isnan(cuda_vals) & np.isnan(sass_vals))
-    if not np.allclose(cuda_vals[mask], sass_vals[mask]):
-      return False
-  return True
+from tinygrad.runtime.ops_cuda import SASSCompiler, SASSRenderer, CUDACompiler
 
 if __name__ == "__main__":
-  CuAsmLogger.initLogger("/home/julian/projects/tinycorp/tinygrad/logs/debug.txt", file_level=15, stdout_level=40)
-
   ast_strs = load_worlds(filter_reduce=False, filter_novariable=True)
-  # no bfloat16 for ptx at the moment
+  # no bfloat16 for sass at the moment
   ast_strs = [x for x in ast_strs if "dtypes.bfloat16" not in x]
   dev = Device["CUDA"]
   sass = SASSRenderer(dev.arch)
 
-  is_debug = (debug_sass := getenv("DEBUG_SASS", "")) or (debug_cubin := getenv("DEBUG_CUBIN", ""))
+  single = getenv("NUM", -1)
+  if single != -1: ast_strs = ast_strs[single:single+1]
 
-  result = defaultdict(list)
-  average_tm_cuda, average_tm_ptx = 0, 0
-  single, start, end, max_nodes = getenv("NUM", -1), getenv("START", 0), getenv("END", len(ast_strs)), getenv("MAX_NODES", -1)
+  average_tm_cuda, average_tm_sass = 0, 0
   for num,ast in enumerate(ast_strs):
-    if (getenv("TEST", 0) and num not in impl) or not (start <= num < end) or (single != -1 and num != single):
-      continue
-
     # cuda compile
     dev.compiler = CUDACompiler(dev.arch)
     lin = ast_str_to_lin(ast, opts=dev.renderer)
     lin.hand_coded_optimizations()
     cuda_prg = CompiledRunner(lin.to_program())
 
+    bufs = bufs_from_lin(lin)
+
+    # sass compile
     dev.compiler = SASSCompiler(dev.arch)
-    lin = ast_str_to_lin(ast, opts=sass if not is_debug else dev.renderer)
+    lin = ast_str_to_lin(ast, opts=sass)
     lin.hand_coded_optimizations()
+    lin.linearize()
+    sass_prg = CompiledRunner(lin.to_program())
 
-    if max_nodes != -1 and len(lin.linearize().uops) > max_nodes: continue
-
+    # warmup
     try:
-      raw_prg = lin.to_program()
-    except Exception as e:
-      print(colored(f"kernel {num}: renderer failure", "red"))
-      print(traceback.format_exc())
+      cuda_prg(bufs, {}, wait=True)
+    except RuntimeError:
+      print("cuda failed ast:", num)
       continue
+    sass_prg(bufs, {}, wait=True)
 
-    # init buffers
-    np.random.seed(222)
-    cuda_bufs = bufs_from_lin(lin)
-    for buf in cuda_bufs:
-      buf.copyin(memoryview(bytearray(np.random.bytes(buf.size * buf.dtype.itemsize))))
-    debug_bufs = [Buffer(buf.device, buf.size, buf.dtype, initial_value=bytearray(buf.as_buffer())) for buf in cuda_bufs]
-
-    if is_debug:
-      if debug_sass:
-        with open(debug_sass) as f: cubin = SASSCompiler(dev.arch).compile(f.read())
-        with open(Path(debug_sass).with_name("debug.cubin"), "wb") as f: f.write(cubin)
-      else:
-        with open(debug_cubin, "rb") as f: cubin = f.read();
-      debug_prg = CompiledRunner(raw_prg, precompiled=cubin)
-      print(f"debug: {debug_prg(debug_bufs, {}, wait=True)*1e6:7.2f} us")
-    else:
-      try:
-        debug_prg = CompiledRunner(raw_prg)
-      except Exception as e:
-        print(colored(f"kernel {num}: assembler failure", "red"))
-        print(traceback.format_exc())
-        continue
-
-    # run programs
-    try:
-      cuda_t, debug_t = cuda_prg(cuda_bufs, {}, wait=True), debug_prg(debug_bufs, {}, wait=True)
-    except Exception as e:
-      print(e)
-      print(colored(f"kernel {num}: runtime failure", "red"))
-      result["runtime_failure"].append(num)
-      continue
-
-    # check if cuda and sass buffers match
-    if allclose(cuda_bufs, debug_bufs):
-      status = colored("success", "green")
-      result["success"].append(num)
-    else:
-      status = colored("mismatch", "red")
-      result["mismatch"].append((num, info(cuda_bufs, debug_bufs)))
-    print(f"{num:>5}/{len(ast_strs)} ({(num + 1) * 100.0 / len(ast_strs):.2f}%){' '*4}"
-          f"cuda: {cuda_t*1e6:7.2f} us{' '*4}sass: {debug_t*1e6:7.2f} us{' '*4}nodes: {len(lin.uops):>3}{' '*4}{status}")
-
-  with open("results.json", "w") as f:
-    json.dump(result, f)
-
-  print(f"{len(result["success"])=}")
-  print(f"{result["compile_failure"]=}")
-  print(f"{result["runtime_failure"]=}")
-  print(f"{result["mismatch"]=}")
+    tm_cuda, tm_sass = [], []
+    for i in range(5):
+      tm_cuda.append(cuda_prg(bufs, {}, wait=True))
+      tm_sass.append(sass_prg(bufs, {}, wait=True))
+    average_tm_cuda += min(tm_cuda)
+    average_tm_sass += min(tm_sass)
+    ratio = min(tm_sass) / min(tm_cuda)
+    print(f"{average_tm_sass / average_tm_cuda:5.2f}x -- {num:4d} {colorize_float(ratio)}  {min(tm_sass) * 1e6:7.2f} us", lin.name)
